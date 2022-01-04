46ff6e53ffc93d1c67e5d18acb3f1e0a4dc8e4df
gy <1838940294@qq.com>
2022-01-04 21:04:07 +0800

    chore: eslint && prettier && stylelint && some loader, plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,62 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StylelintWeboackOlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        // css-loader前者用于解析我们 import 的 css 文件（包括使用 @import 和 url() 引入的文件）
+        // style-loader用于把解析后的样式直接插入到 dom 中，无需在 html 中引入 css 文件
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./public/index.html"),
-      filename: "index.html",
-      title: "TP-任务池",
+      template: path.resolve(__dirname, './public/index.html'),
+      filename: 'index.html',
+      title: 'TP-任务池',
+    }),
+    new StylelintWebpackPlugin({
+      configFile: path.resolve(__dirname, './.stylelintrc.js'),
+      files: ['src/**/*.{less,css}'],
+      customSyntax: 'postcss-less', // 适配 less 语法
+      fix: true, // 自动格式化
+      lintDirtyModulesOnly: true, // 仅检查变化的代码
+      threads: true, // 多线程
     }),
   ],
   cache: {
-    type: "filesystem",
+    type: 'filesystem',
     buildDependencies: {
       config: [__filename],
     },
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.tsx', '.ts', '.js'],
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "dist"),
+      directory: path.resolve(__dirname, 'dist'),
     },
     port: 9527,
     open: true,
