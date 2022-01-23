4de7487eba4540e433a182f04121ddc2e4fbadf7
gy <1838940294@qq.com>
2022-01-23 21:45:54 +0800

    feat: logout and today-panel

--- src/layouts/components/content/index.tsx
+++ src/layouts/components/content/index.tsx
@@ -1,10 +1,9 @@
 import React, { FC, Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from 'antd';
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import Loading from '@/views/sys/loading';
 import { TpViewProps } from '@/types/global';
-import useAuth from '@/hooks/useAuth';
+import TpHeader from './Header';
 
 interface TpContentProps {
   collapsed: boolean;
@@ -14,28 +13,16 @@ interface TpContentProps {
 
 const TpContent: FC<TpContentProps> = (props) => {
   const { collapsed, toggle, viewArr } = props;
-  const { Header, Content } = Layout;
+  const { Content } = Layout;
   const defaultView = viewArr.find((view) => view.default)?.path;
-  const { user } = useAuth();
 
   return (
     <Layout className="auth-app-layout">
-      <Header
-        className="app-layout-header"
-        style={{ position: 'relative', padding: 0, background: '#ffffff', boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)' }}
-      >
-        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-          className: 'trigger',
-          onClick: toggle,
-        })}
-        <div className="tp-avatar">
-          <img src={user?.avatar} alt={user?.username} />
-        </div>
-      </Header>
+      <TpHeader collapsed={collapsed} toggle={toggle} />
       <Content className="app-layout-content">
         <Suspense fallback={<Loading />}>
           <Routes>
-            <Route path="/taskponds" element={<Navigate to={`/${defaultView}`} />} />
+            <Route path="/" element={<Navigate to={`/${defaultView}`} />} />
             {viewArr.map((view) => {
               return <Route key={view.id} path={view.path} element={view.element} />;
             })}
