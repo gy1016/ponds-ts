9e12cfaaed1c97730f3994c8988249e75e5c42ee
gy <1838940294@qq.com>
2022-01-23 17:34:38 +0800

    build: delete notes in master webpack config

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -6,49 +6,30 @@ const { merge } = require('webpack-merge');
 module.exports = merge(base, {
   mode: 'development',
   output: {
-    // 一定记得加斜线
     sourceMapFilename: 'sourcemap/[name].[contenthash].js.map',
   },
   plugins: [
     new StylelintWebpackPlugin({
       configFile: path.resolve(__dirname, '../.stylelintrc.js'),
       files: ['src/**/*.{less,css}'],
-      customSyntax: 'postcss-less', // 适配 less 语法
-      fix: true, // 自动格式化
-      lintDirtyModulesOnly: true, // 仅检查变化的代码
-      threads: true, // 多线程
+      customSyntax: 'postcss-less',
+      fix: true,
+      lintDirtyModulesOnly: true,
+      threads: true,
     }),
   ],
-  // 将打包后的bundle放到了内存里，自己没有输出任何东西
-  // 删掉dist文件也是正常运行的
   devServer: {
-    // 指向当前服务的物理路径
     static: {
-      directory: path.resolve(__dirname, '../dist'), // 告诉服务器从指定目录中提供静态文件，也即打包后的文件
+      directory: path.resolve(__dirname, '../dist'),
     },
     compress: true,
-    port: 9527, // 设置端口
-    open: true, // 自动打开浏览器
-    hot: true, // 开启热更新
-    // 其他小伙伴可以通过访问我得服务
-    // host: '0.0.0.0',
-    // 自定义响应头
-    // headers: {
-    //   'X-Access-Token': '',
-    // },
+    port: 9527,
+    open: true,
+    hot: true,
     proxy: {
       // '/api': 'http://localhost:9000',
     },
-    // https: true,
     historyApiFallback: true,
   },
-  // 默认source-map为eval
-  // 不会单独把source-map文件打包出来，会在末尾添加类似base64链接，可以锁定代码行数
-  // eval-source-map不会生成source-map文件，也可以锁定代码行数
-  // cheap-source-map——只记录行树，不记录列数
-  // hidden-source-map会取消掉在末尾追加的那一行source-url，不能在锁定行数，但会生成map文件，且与压缩后代码没有关联
-  // source-map——行数列数都记录
-  // 开发环境推荐！！！！
-  // cheap-module-source-map——单独生成map文件，而且不记录列数，并且对于babel这种解析的source-map混杂进来以后，也不会导致代码行数的一个识别问题
   devtool: 'cheap-module-source-map',
 });
--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -8,39 +8,23 @@ const CompressionWebpackPlugin = require('compression-webpack-plugin');
 module.exports = merge(base, {
   mode: 'production',
   output: {
-    // 一定记得加斜线
     // publicPath: 'http://localhost:8080/',
-    // sourceMapFilename: 'sourcemap/[name].[contenthash].js.map',
   },
   plugins: [
     new BundleAnalyzerPlugin({
-      analyzerMode: 'disabled', // 不启用分析器
+      analyzerMode: 'disabled',
       generateStatsFile: true,
       statsFilename: 'stats.json',
     }),
     new CompressionWebpackPlugin({
       test: /\.(js|tff|css|html|svg|png)$/,
-      threshold: 10240, // 对超过10k的数据压缩
+      threshold: 10240,
     }),
-    // 与base中是两个对象不会合并
-    // new HtmlWebpackPlugin({
-    //   cdn: {
-    //     js: [
-    // 'https://unpkg.com/react@17/umd/react.production.min.js',
-    // 'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
-    // 'https://cdn.jsdelivr.net/npm/axios@0.24.0/dist/axios.min.js',
-    // 'https://cdn.jsdelivr.net/npm/react-router-dom@6.0.2/umd/react-router-dom.production.min.js',
-    // 'https://cdn.bootcdn.net/ajax/libs/moment.js/2.29.1/locale/af.min.js',
-    //     ],
-    //   },
-    // }),
   ],
   optimization: {
     minimize: true,
     minimizer: [
-      // 压缩CSS代码
       new CssMinimizerPlugin(),
-      // 压缩JS代码
       new TerserPlugin({
         terserOptions: {
           compress: {
@@ -50,19 +34,10 @@ module.exports = merge(base, {
       }),
     ],
   },
-  // 使用这种方式引入CDN，index.html里并没有cdn的链接引入，
-  // 是在运行时才引入的
   externalsType: 'script',
   externals: {
-    // react: 'React',
-    // 'react-dom': 'ReactDOM',
     axios: ['https://cdn.jsdelivr.net/npm/axios@0.24.0/dist/axios.min.js', 'axios'],
-    // 'react-router-dom': 'ReactRouterDOM',
-    // moment: ['https://cdn.bootcdn.net/ajax/libs/moment.js/2.29.1/locale/af.min.js', 'moment'],
   },
-  // 生产环境不推荐有source-map
-  // devtool: 'source-map',
-  // 把打包过程中的性能提示去掉
   performance: {
     hints: false,
   },
