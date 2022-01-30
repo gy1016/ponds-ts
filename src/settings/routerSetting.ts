54c28f530d3d25dfcced340b05b5ee3eec83674b
gy <1838940294@qq.com>
2022-01-30 10:52:28 +0800

    fix: add dist to gitignore

--- src/settings/routerSetting.ts
+++ src/settings/routerSetting.ts
@@ -6,19 +6,19 @@ const TpRouterArr: Array<TpRouterProps> = [
   {
     id: 1,
     label: '面板',
-    path: 'task',
+    path: 'taskponds/task',
     icon: createElement(SnippetsOutlined),
   },
   {
     id: 2,
     label: '分析',
-    path: 'analysis',
+    path: 'taskponds/analysis',
     icon: createElement(LineChartOutlined),
   },
   {
     id: 3,
     label: '今日',
-    path: 'today',
+    path: 'taskponds/today',
     icon: createElement(EditOutlined),
   },
 ];
