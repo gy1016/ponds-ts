01a6f6c51cec92d4791618239a44ec506016025e
gy <1838940294@qq.com>
2022-02-22 09:09:43 +0800

    feat: error-boundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React, { FC } from 'react';
 import AuthenticatedApp from '@/layouts';
 import UnauthenticatedApp from '@/views/sys/unauthenticated-app';
 import useAuth from '@/hooks/useAuth';
+import { ErrorBoundary } from './components/error-boundary';
+import ErrorPage from './views/sys/error';
 import './App.less';
 
 const App: FC = () => {
   const { user } = useAuth();
 
-  return <div id="app">{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</div>;
+  return (
+    <div id="app">
+      <ErrorBoundary fallbackRender={ErrorPage}>{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</ErrorBoundary>
+    </div>
+  );
 };
 
 export default App;
